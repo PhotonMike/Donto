e6e72d236c77667c0efcd8f310634374d2dd1df1
PhotonMike <tothmiklostibor@gmail.com>
2017-05-20 00:00:31 +0200

    Kotlin1

---
+++ src/hu/kockak/KotlinMain.kt
@@ -0,0 +1,16 @@
+package hu.kockak
+
+import java.util.*
+import kotlin.repeat
+
+class KotlinMain {
+    fun main (){
+        var konzol = Scanner(System.`in`)
+        var bemenet = konzol.nextLine()
+        var számok = IntArray(10000)
+        var g=0
+        var akt = ""
+        var vege=0
+        repeat(bemenet.length)
+    }
+}
